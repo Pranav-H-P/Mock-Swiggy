22bc907f1cdfd5584015555702a95fa75c2f08b2
Pranav <pranavhp871@gmail.com>
2024-11-04 16:46:39 +0530

    Created footer and made it responsive

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -9,4 +9,49 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  footerLinkTitles = [
+      "Company",
+      "Contact Us",
+      "Available in:",
+      "Life at Swigular",
+      "Legal"
+  ];
+  footerLinks = [
+
+    [
+      "About Us",
+      "Swigular Corporate",
+      "Careers",
+      "Team",
+      "Swigular One",
+      "Swigular Instamart",
+      "Swigular Dineout",
+      "Swigular Genie"
+    ],
+    [
+      "Help & Support",
+      "Partner with us",
+      "Ride with us"
+    ],
+    [
+      "Bangalore",
+      "Gurgaon",
+      "Hyderabad",
+      "Delhi",
+      "Mumbai",
+      "Pune"
+    ],
+    [
+      "Explore with Swigular",
+      "Swigular News",
+      "Snackables"
+    ],
+    [
+      "Terms & Conditions",
+      "Cookie Policy",
+      "Privacy Policy",
+      "Investor Relations"
+    ]
+
+  ];
 }
