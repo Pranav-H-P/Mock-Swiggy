08838900357c10da302ee7f6ff55917d09350519
Pranav <pranavhp871@gmail.com>
2024-11-05 00:31:16 +0530

    Completed Home page, Now restaurant page, checkout/payment page and search is left

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,11 +5,12 @@ import { RestaurantServiceService } from '../services/restaurant-service.service
 import { FoodItemType, RestaurantType } from '../item-types';
 import { FoodItemCardComponent } from "../reuseable/food-item-card/food-item-card.component";
 import { DataServiceService } from '../services/data-service.service';
+import { RestaurantCardComponent } from "../reuseable/restaurant-card/restaurant-card.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FoodItemCardComponent],
+  imports: [FoodItemCardComponent, RestaurantCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -30,8 +31,9 @@ export class HomeComponent {
       this.dataService.setCurrFood(foodName);
       this.router.navigate(['foodPage']);
     }
-    onRestaurantCardClicked(foodName: string){
-
+    onRestaurantCardClicked(restName: string){
+      this.dataService.setCurrFood(restName);
+      this.router.navigate(['restaurantPage']);
     }
 
 }
